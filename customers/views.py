825acefd6ee3f1513ad77771727633b961384c4c
Dr-dyrane <halodyrane@gmail.com>
2022-12-13 13:09:38 +0100

    404

--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,19 @@
 from .models import *
 from .serializers import *
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 
 def customers(request):
   data = Customer.objects.all()
   serializer = CustomerSerializer(data, many=True)
   context = {'customers': serializer.data}
   return JsonResponse(context)
+
+def customer(request,id):
+  try:
+    data = Customer.objects.get(pk=id)
+  except Customer.DoesNotExist:
+    raise Http404('Customer does not exist')
+  serializer = CustomerSerializer(data)
+  context = {'customer': serializer.data}
+  return JsonResponse(context)
+
